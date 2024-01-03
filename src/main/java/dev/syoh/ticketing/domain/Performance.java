af9a778f6307dc0688c6035c6edc7e7423d9ec1f
ohahohah <ohahohah.dev@gmail.com>
2024-01-03 19:12:32 +0900

    [refactor]불필요한 Getter, Setter, 생성자 삭제

--- src/main/java/dev/syoh/ticketing/domain/Performance.java
+++ src/main/java/dev/syoh/ticketing/domain/Performance.java
@@ -1,23 +1,36 @@
 package dev.syoh.ticketing.domain;
 
+import dev.syoh.ticketing.dto.PerformanceDto;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
-@RequiredArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor
 public class Performance {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String name;
+
+	public Performance(String name) {
+		this.name = name;
+	}
+
+	public void changeName(String name) {
+		this.name = name;
+	}
+
+	public PerformanceDto convertToDto() {
+		return new PerformanceDto(this.name);
+	}
+
+
 }
 
--- src/main/java/dev/syoh/ticketing/dto/PerformanceDto.java
+++ src/main/java/dev/syoh/ticketing/dto/PerformanceDto.java
@@ -1,17 +1,22 @@
 package dev.syoh.ticketing.dto;
 
 import dev.syoh.ticketing.domain.Performance;
+import lombok.NoArgsConstructor;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
+@NoArgsConstructor
 public class PerformanceDto {
 	private long id;
 	private String name;
 
+	public PerformanceDto(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
 	public Performance toEntity() {
-		return new Performance(id, name);
+		return new Performance(name);
 	}
 }
